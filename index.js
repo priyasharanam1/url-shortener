acc47e6f67e072d1f876a7acd20e560ca670984a
priyasharanam1 <p1910413219@gmail.com>
2023-11-05 19:04:43 +0530

    added simple ui

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const urlRoute = require("./routes/url");
 const {connectToMongoDB} = require("./connect");
 const URL = require("./models/url");
+const path = require("path")
+const staticRoute = require("./routes/staticRouter")
 
 const app = express();
 const PORT = 8001;
@@ -9,18 +11,33 @@ const PORT = 8001;
 connectToMongoDB("mongodb://127.0.0.1:27017/short-url")
 .then(() => console.log("mongoDB connected"));
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"))
+
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+// app.get("/test", async(req,res) => {
+//     const allUrls = await URL.find({});
+//     return res.render("home", {
+//         urls : allUrls,
+//     })
+// })
 
 app.use("/url", urlRoute);
+app.use("/", staticRoute);
 
-app.get("/:shortId", async(req,res) => {
+app.get("/url/:shortId", async(req,res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortId
     }, { $push: {
         visitHistory: { timestamp: Date.now()},
     } });
-    res.redirect(entry.redirectURL);
+    if (entry && entry.redirectURL) {
+        res.redirect(entry.redirectURL);
+      } 
 })
 
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
